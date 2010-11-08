ab10e264a88a77f98180287adf78475aff703ead
Nathan Wilcox <nejucomo@gmail.com>
2010-11-07 23:53:39 -0800

    Bump the release number for the new AutoImporter interface.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='pyeval',
       description='Conveniently evaluate expressions from the shell',
-      version='0.1',
+      version='0.1.1',
       author='Nathan Wilcox',
       author_email='nejucomo@gmail.com',
       license='GPLv3',
