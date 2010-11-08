94cea8fe0bcbabc96eb684267d6266e727c23219
Nathan Wilcox <nejucomo@gmail.com>
2010-11-07 22:33:16 -0800

    Rename script to pyeval, create a distutils installer.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyeval',
+      version='0.1',
+      description='Conveniently evaluate expressions from the shell',
+      author='Nathan Wilcox',
+      author_email='nejucomo@gmail.com',
+      url='https://bitbucket.org/nejucomo/pyeval/',
+      scripts=['pyeval'],
+     )
