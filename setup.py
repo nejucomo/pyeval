ef50d54491024addeb7daec827feddab25d5ccef
Nathan Wilcox <nejucomo@gmail.com>
2010-11-07 23:16:50 -0800

    Add licensing details; MANIFEST.in; ignore setup output.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='pyeval',
-      version='0.1',
       description='Conveniently evaluate expressions from the shell',
+      version='0.1',
       author='Nathan Wilcox',
       author_email='nejucomo@gmail.com',
+      license='GPLv3',
       url='https://bitbucket.org/nejucomo/pyeval/',
       scripts=['pyeval'],
      )
