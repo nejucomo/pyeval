6ba1aad8b94caac0632bc81cde36fe68941145c4
Nathan Wilcox <nejucomo@gmail.com>
2013-07-02 22:02:01 -0700

    Rename pyeval -> pyeval.py - a library, then add a bare "pyeval" script.

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from distutils.core import setup
 
 setup(name='pyeval',
       description='Conveniently evaluate expressions from the shell',
-      version='0.1.6',
+      version='0.1.6-dev',
       author='Nathan Wilcox',
       author_email='nejucomo@gmail.com',
       license='GPLv3',
       url='https://bitbucket.org/nejucomo/pyeval/wiki/Home',
       scripts=['pyeval'],
-     )
+      py_modules=['pyeval'],
+      )
