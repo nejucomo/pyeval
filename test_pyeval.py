dc4f41052ae68f00e39d66f11004b56737960f38
Nathan Wilcox <nejucomo@gmail.com>
2013-07-02 22:45:22 -0700

    Add AutoImporter tests; child handlers fail suggesting a misunderstanding in the import mechanism.

--- test_pyeval.py
+++ test_pyeval.py
@@ -8,6 +8,38 @@ import pyeval
 
 
 
+class AutoImporterTests (unittest.TestCase):
+    def setUp(self):
+        import logging
+        self.logging = logging
+        from logging import handlers
+        self.handlers = handlers
+
+        self.parent = pyeval.AutoImporter('logging')
+        self.child = self.parent.handlers
+
+    def test__ai_mod(self):
+        self.assertIs(self.logging, self.parent._ai_mod)
+        self.assertIs(self.handlers, self.child._ai_mod)
+
+    def test__ai_parent(self):
+        self.assertIs(None, self.parent._ai_parent)
+        self.assertIs(self.logging, self.child._ai_parent)
+
+    def test__ai_name(self):
+        self.assertEqual('logging', self.parent._ai_name)
+        self.assertEqual('handlers', self.child._ai_name)
+
+    def test__ai_fullname(self):
+        self.assertEqual('logging', self.parent._ai_fullname)
+        self.assertEqual('logging.handlers', self.child._ai_fullname)
+
+    def test_attr(self):
+        self.assertIs(self.logging.basicConfig, self.parent.basicConfig)
+        self.assertIs(self.handlers.MemoryHandler, self.child.MemoryHandler)
+
+
+
 class MagicScopeTests (unittest.TestCase):
     def setUp(self):
         self.scope = pyeval.MagicScope()
@@ -21,6 +53,10 @@ class MagicScopeTests (unittest.TestCase):
             self.assertEqual(stripin, self.scope['i'])
             self.assertEqual(rawin, self.scope['ri'])
             self.assertEqual(stripin, self.scope['i'])
+
+    def test_AutoImporterHook(self):
+        # Pick a module that is not imported by default:
+        self.assertIsInstance(self.scope['BaseHTTPServer'], pyeval.AutoImporter)
 
 
 
