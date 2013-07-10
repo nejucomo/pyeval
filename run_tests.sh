21d235dd6f785f3e6129001eb99533a7542110c6
Nathan Wilcox <nejucomo@gmail.com>
2013-07-10 09:12:10 -0700

    Only generate coverage reports for pyeval.

--- run_tests.sh
+++ run_tests.sh
@@ -9,12 +9,12 @@ then
     echo 'Could not find trial; it is in the Twisted package.'
     exit -1
 fi
-    
+
 
 PYTHONPATH="./lib:$PYTHONPATH" coverage run "$TRIAL" ./lib/pyeval
 STATUS=$?
 
-coverage html
+coverage html --include='lib/pyeval/*'
 
 exit $STATUS
 
