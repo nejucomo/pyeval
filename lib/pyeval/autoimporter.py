ebfaf3ef372832fca6e92544c6b1db80ef2dccd8
Nathan Wilcox <nejucomo@gmail.com>
2013-07-10 10:03:36 -0700

    Export autoimporter from everything.

---
+++ lib/pyeval/autoimporter.py
@@ -0,0 +1,54 @@
+__all__ = ['AutoImporter', 'import_last']
+
+
+from types import ModuleType
+
+
+
+def import_last(modpath):
+    mod = __import__(modpath)
+    for name in modpath.split('.')[1:]:
+        mod = getattr(mod, name)
+    return mod
+
+
+class AutoImporter (object):
+    def __init__(self, mod):
+        assert type(mod) is ModuleType, `mod`
+
+        self._ai_mod = mod
+
+        try:
+            path = mod.__file__
+        except AttributeError:
+            self._ai_path = None
+        else:
+            if path.endswith('.pyc'):
+                path = path[:-1]
+
+            self._ai_path = path
+
+
+    @property
+    def _ai_name(self):
+        return self._ai_mod.__name__
+
+    def __repr__(self):
+        modrepr = repr(self._ai_mod)
+        assert modrepr.startswith('<') and modrepr.endswith('>'), modrepr
+        return '<%s for %s>' % (self.__class__.__name__, modrepr[1:-1])
+
+    def __getattr__(self, name):
+        try:
+            x = getattr(self._ai_mod, name)
+        except AttributeError, outerError:
+            try:
+                x = import_last(self._ai_name + '.' + name)
+            except ImportError:
+                raise outerError
+
+        if type(x) is ModuleType:
+            return AutoImporter(x)
+        else:
+            return x
+
--- lib/pyeval/everything.py
+++ lib/pyeval/everything.py
@@ -3,10 +3,10 @@
 import __builtin__
 import sys
 import pprint
-from types import ModuleType
 from functools import wraps
 
 from pyeval import display
+from pyeval.autoimporter import AutoImporter, import_last
 from pyeval.help import HelpBrowser
 
 
@@ -15,13 +15,6 @@ def pyeval(expr, *args):
     scope = MagicScope()
     scope.registerArgsMagic(args)
     return eval(expr, {}, scope)
-
-
-def import_last(modpath):
-    mod = __import__(modpath)
-    for name in modpath.split('.')[1:]:
-        mod = getattr(mod, name)
-    return mod
 
 
 def fallthroughDefault(key):
@@ -246,46 +239,3 @@ class MagicScope (dict):
             else:
                 value = self[key] = method()
                 return value
-
-
-
-class AutoImporter (object):
-    def __init__(self, mod):
-        assert type(mod) is ModuleType, `mod`
-
-        self._ai_mod = mod
-
-        try:
-            path = mod.__file__
-        except AttributeError:
-            self._ai_path = None
-        else:
-            if path.endswith('.pyc'):
-                path = path[:-1]
-
-            self._ai_path = path
-
-
-    @property
-    def _ai_name(self):
-        return self._ai_mod.__name__
-
-    def __repr__(self):
-        modrepr = repr(self._ai_mod)
-        assert modrepr.startswith('<') and modrepr.endswith('>'), modrepr
-        return '<%s for %s>' % (self.__class__.__name__, modrepr[1:-1])
-
-    def __getattr__(self, name):
-        try:
-            x = getattr(self._ai_mod, name)
-        except AttributeError, outerError:
-            try:
-                x = import_last(self._ai_name + '.' + name)
-            except ImportError:
-                raise outerError
-
-        if type(x) is ModuleType:
-            return AutoImporter(x)
-        else:
-            return x
-
