d6eba98aece971a5df9a2df55608becdf54ebd40
Nathan Wilcox <nejucomo@gmail.com>
2013-07-10 09:30:19 -0700

    Move pyeval.py -> pyeval/everything.py and start adding imports in pyeval/__init__.py to fix unittests.

---
+++ lib/pyeval/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    'AutoImporter',
+    'HelpBrowser',
+    'MagicScope',
+    'Usage',
+    'main',
+    'pyeval',
+    'import_last',
+    ]
+
+from pyeval.everything import \
+    AutoImporter, \
+    HelpBrowser, \
+    MagicScope, \
+    Usage, \
+    main, \
+    pyeval, \
+    import_last
