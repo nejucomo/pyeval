80ab52255a7804afc418aa348a82a56f8c785c75
Nathan Wilcox <nejucomo@gmail.com>
2013-07-10 09:39:33 -0700

    Export main from everything; add missing modules from previous two commits.

---
+++ lib/pyeval/main.py
@@ -0,0 +1,14 @@
+__all__ = ['main']
+
+import sys
+
+from pyeval.display import displayPretty
+from pyeval.everything import pyeval
+
+
+def main(args = sys.argv[1:], displayhook=displayPretty):
+    sys.displayhook = displayhook
+
+    expr, strs = args[0], args[1:]
+    result = pyeval(expr, *strs)
+    sys.displayhook(result)
