915aac367f0ab7b1fb44d4418e95d9c2904c5846
Nathan Wilcox <nejucomo@gmail.com>
2013-07-11 08:16:09 -0700

    Add test spec for -h and --help aliases.

--- lib/pyeval/tests/test_main.py
+++ lib/pyeval/tests/test_main.py
@@ -14,3 +14,20 @@ class pyevalTests (unittest.TestCase):
         self.assertEqual('42\n', fio.fakeout.getvalue())
         self.assertEqual('', fio.fakeerr.getvalue())
 
+    def test_helpAlias(self):
+        fio = FakeIO()
+        with fio:
+            main(['help'])
+
+        expected = fio.fakeout.getvalue()
+        self.assertEqual('', fio.fakeerr.getvalue())
+
+        for alias in ['-h', '--help']:
+            fio = FakeIO()
+            with fio:
+                main([alias])
+
+            self.assertEqual(expected, fio.fakeout.getvalue())
+            self.assertEqual('', fio.fakeerr.getvalue())
+
+
