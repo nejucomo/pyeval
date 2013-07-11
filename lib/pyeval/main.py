98960197090c0dc9334f10f6f4350c4ae0978d15
Nathan Wilcox <nejucomo@gmail.com>
2013-07-11 08:18:30 -0700

    Implement the help alias test spec.

--- lib/pyeval/main.py
+++ lib/pyeval/main.py
@@ -6,5 +6,9 @@ from pyeval.eval import pyevalAndDisplay
 
 
 def main(args = sys.argv[1:]):
+
+    if len(args) == 0 or args[0] in ['-h', '--help']:
+        args = ['help']
+
     pyevalAndDisplay(*args)
 
